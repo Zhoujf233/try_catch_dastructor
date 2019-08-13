b07801fc9a1c56309bd1360c9f51530ba816cdfe
zhoujf <zhoujf1636@163.com>
2019-08-13 20:16:42 +0800

    Initial Commit

---
+++ try_catch_destructor/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  try_catch_destructor
+//
+//  Created by JF Zhou on 2019/8/13.
+//  Copyright © 2019 JF Zhou. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
